f8807f783e8763a240b45271108ac0be679b70d8
Mahavirsinh Chauhan <100768509+mahavir-amoga@users.noreply.github.com>
2023-07-13 09:43:45 +0530

    adding navigation

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar, StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle='light-content' />
-      <CategoriesScreen />
+      <StatusBar barStyle='dark-content' backgroundColor='#FFF' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
